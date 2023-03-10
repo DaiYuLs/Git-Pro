89f735e692dc9e253e5062f29a274f0db05d6a34
代渝 <1198863970@qq.com>
2023-03-10 11:34:05 +0800

    第一次提交到本地仓库

---
+++ src/main/java/com/atguigu/git/MyTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: MyTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author 尚硅谷-代渝
+ * @Create 2023/3/10 11:32
+ * @Version 1.0
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
